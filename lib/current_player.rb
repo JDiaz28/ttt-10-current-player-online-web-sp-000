bbd91ca73a473c33653e2d7113d039422e952b62
Abraham Moreno <adiaz2821@gmail.com>
2020-05-28 22:10:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   turn = 0
   counter = 0
   board.each do |turn|
-      if board == "X" || board == "O"
+    if board == "X" || board == "O"
       counter += 1 
     end 
   end 
